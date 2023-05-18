36d6245ab86b10521329845b895aa0f5fe23e5b8
Lehlohonolo Moloi <Lehlohonolo@bbd.co.za>
2023-05-18 15:21:37 +0200

    Removed dead code

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,10 +1,7 @@
 module.exports = (app) => {
     const movieController = require('../controllers/movieController');
-    const userAuthentication = require('../controllers/userAuthentication');
 
     app.get('/movies', movieController.findAll);
     app.get('/movies/:genre', movieController.findByGenre);
-    app.get('/movies/year', movieController.findByYear);
     app.get('/movies/external/search', movieController.externalAPI);
-    app.get('/user/credentials', userAuthentication.authenticate);
 }
