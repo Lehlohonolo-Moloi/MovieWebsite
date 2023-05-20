13af2e25fa8476532cfe3850e14bfa44906a6c3f
Lehlohonolo Moloi <Lehlohonolo@bbd.co.za>
2023-05-21 01:30:38 +0200

    Added code to sign user in using google oauth, and code to make it possible to sign out and check if a user is not signed in.

---
+++ site/js/login.js
@@ -0,0 +1,26 @@
+function signIn(){
+    let oauth2Endpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+
+    let form = document.createElement('form');
+    form.setAttribute('method', 'GET');
+    form.setAttribute('action', oauth2Endpoint);
+
+    let params = {
+        "client_id":"910075582560-bnqcom7c276kc5iliev9b2o6a0uo6b3a.apps.googleusercontent.com",
+        "redirect_uri":"http://127.0.0.1:5500/site/index.html",
+        "response_type":"token",
+        "scope": "https://www.googleapis.com/auth/userinfo.profile",
+        "include_granted_scopes":'true'
+    }
+
+    for(var p in params){
+        let input = document.createElement('input');
+        input.setAttribute('type', 'hidden');
+        input.setAttribute('name', p);
+        input.setAttribute('value', params[p]);
+        form.appendChild(input);
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
---
+++ site/js/logout.js
@@ -0,0 +1,34 @@
+let params = {};
+
+    let regex = /([^&=]+)=([^&]*)/g, m;
+
+    //get url info 
+    while(m = regex.exec(location.href)){
+      params[decodeURIComponent(m[1])] = decodeURIComponent(m[2]);
+    }
+
+    //check if user is logged in
+    if(Object.keys(params).length > 0){
+      localStorage.setItem('authInfo',JSON.stringify(params));
+    }
+    else{
+        location.href = "http://127.0.0.1:5500/site/login.html";
+    }
+
+    //hide access token (mask url)
+    window.history.pushState({}, document.title, "/site/index.html");
+
+    let info = JSON.parse(localStorage.getItem('authInfo'));
+ 
+    function logout(){
+      fetch("https://oauth2.googleapis.com/revoke?token=" + info['access_token'], {
+        method:'POST',
+        headers:{
+          'Content-type':'application/x-www-form-urlencoded'
+        }
+      })
+      .then((data) => {
+        location.href = "http://127.0.0.1:5500/site/login.html";
+      })
+    }
+
