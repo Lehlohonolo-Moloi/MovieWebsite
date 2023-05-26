ba60e7d34050652c043abc1252784e78a2a2f663
Lehlohonolo-Moloi <122453621+Lehlohonolo-Moloi@users.noreply.github.com>
2023-05-26 14:47:47 +0200

    Update logout.js
    Updated the user session and logout.

--- site/js/logout.js
+++ site/js/logout.js
@@ -10,19 +10,29 @@ let params = {};
     //check if user is logged in
     if(Object.keys(params).length > 0){
       localStorage.setItem('authInfo',JSON.stringify(params));
-      let data = JSON.parse(localStorage.getItem('authInfo'));
-      localStorage.setItem('state', data['http://127.0.0.1:5500/site/index.html#state']);
+      let info = JSON.parse(localStorage.getItem('authInfo'));
+      localStorage.setItem('state', info['http://127.0.0.1:5500/site/index.html#state']);
+      
+      fetch("https://www.googleapis.com/oauth2/v3/userinfo",{ 
+        headers:{ "Authorization":`Bearer ${info['access_token']}` 
+      } }) 
+      .then((data) => data.json()) 
+      .then((userInfo) => { 
+        localStorage.setItem('username',userInfo['name']);
+      });
+
     }
     if (localStorage.getItem('state') !== 'LoggedIn'){
         location.href = "http://127.0.0.1:5500/site/login.html";
     }
 
+    document.getElementById("userInfoHome").innerText = localStorage.getItem('username');
+
     //hide access token (mask url)
     window.history.pushState({}, document.title, "/site/index.html");
-
-    let info = JSON.parse(localStorage.getItem('authInfo'));
  
     function logout(){
+      let info = JSON.parse(localStorage.getItem('authInfo'));
       fetch("https://oauth2.googleapis.com/revoke?token=" + info['access_token'], {
         method:'POST',
         headers:{
@@ -35,4 +45,3 @@ let params = {};
 
       localStorage.setItem('state', "LoggedOut");
     }
-
